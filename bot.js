929404e490f0c1b7547a36ea884a9b248e4fbb50
Berserker09 <93809440+berserker09@users.noreply.github.com>
2021-11-27 13:57:06 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,25 +19,25 @@ async function iniciar () {
         fs.writeFileSync('./session', JSON.stringify(senpai.base64EncodedAuthInfo(), null, '\t'))
         
 
-senpai.on('chat-update', async (sam) => {
+senpai.on('chat-update', async (mek) => {
 try {	  
-if (!sam.hasNewMessage) return
-if (!sam.messages) return
-if (sam.key && sam.key.remoteJid == 'status@broadcast') return
+if (!mek.hasNewMessage) return
+if (!mek.messages) return
+if (mek.key && mek.key.remoteJid == 'status@broadcast') return
 
-sam = sam.messages.all()[0]
-if (!sam.message) return
+mek = mek.messages.all()[0]
+if (!mek.message) return
 global.blocked
-sam.message = (Object.keys(sam.message)[0] === 'ephemeralMessage') ? sam.message.ephemeralMessage.message : sam.message
-const from = sam.key.remoteJid
-const type = Object.keys(sam.message)[0]        
-const quoted = type == 'extendedTextMessage' && sam.message.extendedTextMessage.contextInfo != null ? sam.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+const from = mek.key.remoteJid
+const type = Object.keys(mek.message)[0]        
+const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
 const typeQuoted = Object.keys(quoted)[0]
-const content = JSON.stringify(sam.message)
+const content = JSON.stringify(mek.message)
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
-const body = sam.message.conversation || sam.message[type].caption || sam.message[type].text || ""
-chats = (type === 'conversation') ? sam.message.conversation : (type === 'extendedTextMessage') ? sam.message.extendedTextMessage.text : ''
-budy = (type === 'conversation' && sam.message.conversation.startsWith(prefix)) ? sam.message.conversation : (type == 'imageMessage') && sam.message.imageMessage.caption.startsWith(prefix) ? sam.message.imageMessage.caption : (type == 'videoMessage') && sam.message.videoMessage.caption.startsWith(prefix) ? sam.message.videoMessage.caption : (type == 'extendedTextMessage') && sam.message.extendedTextMessage.text.startsWith(prefix) ? sam.message.extendedTextMessage.text : ''
+const body = mek.message.conversation || mek.message[type].caption || mek.message[type].text || ""
+chats = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
+budy = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : ''
 
 if (prefix != "") {
 if (!body.startsWith(prefix)) {
@@ -62,16 +62,16 @@ const soyYo = senpai.user.jid
 const botNumber = senpai.user.jid.split("@")[0]
 const ownerNumber = ['522213261679@s.whatsapp.net']
 const isGroup = from.endsWith('@g.us')
-const sender = sam.key.fromMe ? senpai.user.jid : isGroup ? sam.participant : sam.key.remoteJid
+const sender = mek.key.fromMe ? senpai.user.jid : isGroup ? mek.participant : mek.key.remoteJid
 const senderNumber = sender.split("@")[0]
 const isMe = senderNumber == botNumber
-const conts = sam.key.fromMe ? senpai.user.jid : senpai.contacts[sender] || { notify: jid.replace(/@.+/, '') }
-const pushname = sam.key.fromMe ? senpai.user.name : conts.notify || conts.vname || conts.name || '-'
+const conts = mek.key.fromMe ? senpai.user.jid : senpai.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+const pushname = mek.key.fromMe ? senpai.user.name : conts.notify || conts.vname || conts.name || '-'
 
 switch (command) {
 
 case 'bot':
-client.sendMessage(from, 'Hola,felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted : sam, sendEphemeral: true})
+client.sendMessage(from, 'Hola,felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted : mek, sendEphemeral: true})
 break
                 
 }
